4c33e3c522398f402b3594d8f508f02cc6e2e8e8
ClaudemirMenezes <claudemir0088@gmail.com>
2024-05-07 21:54:42 -0300

    Foi criado um função para destacar uma descrição de uma determinda palavra em algum texto

---
+++ script.js
@@ -0,0 +1,9 @@
+const descricao = document.getElementById('descricao');
+
+function mostrarDescricao() {
+    descricao.style.display = 'block';
+}
+
+function esconderDescricao() {
+    descricao.style.display = 'none';
+}
